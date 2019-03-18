e0eb787f668e2af1b24c510d5ce25a5084a93d84
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-03-18 10:28:16 +0100

    feat(tag-recipes): remove all other security schemes but BearerAuth

--- server/migrations/20190315135503-add-tags-to-recipes.js
+++ server/migrations/20190315135503-add-tags-to-recipes.js
@@ -1,12 +1,8 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('Recipes', 'tags', {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.addColumn('Recipes', 'tags', {
       type: Sequelize.ARRAY(Sequelize.STRING),
       defaultValue: []
-    });
-  },
-
-  down: queryInterface => {
-    return queryInterface.removeColumn('Recipes', 'tags');
-  }
+    }),
+  down: queryInterface => queryInterface.removeColumn('Recipes', 'tags')
 };
